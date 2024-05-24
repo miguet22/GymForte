b623865e297971609a15b097f51cc73413bc0ca5
miguet22 <joamigue22@gmail.com>
2024-05-24 20:13:11 -0300

    realizado lo de la api

--- js/consumir.js
+++ js/consumir.js
@@ -1,27 +1,29 @@
-const { createApp } = Vue;
+const { createApp } = Vue
 createApp({
   data() {
     return {
-      url: "./js/api.json",
+      url: "./js/planes.json",
       datos: [],
       error: false,
-    };
+    }
   },
   methods: {
     fetchData(url) {
       fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          this.datos = data;
-        })
-        .catch((error) => {
-          console.log("Error:" + error);
-          this.error = true;
+        .then(response => response.json())
+        .then(
+          data => {
+            console.log(data)
+            this.datos = data
+          }
+        )
+        .catch(error => {
+          console.log("Error:" + error)
+          this.error = true
         });
-    },
-  },
-  created() {
-    this.fetchData(this.url);
-  },
-}).mount("#app");
+    }
+  },   
+created() {  // created() se ejecuta cada vez que se crea el objeto VUE
+  this.fetchData(this.url)
+}
+}).mount('#app')
